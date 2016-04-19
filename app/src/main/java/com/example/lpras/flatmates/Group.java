17fbd59fd4cec728e47d5b2c269c332e4ee3dc2d
Prashant Lokhande <lprashant94@gmail.com>
2016-04-19 15:31:48 +0530

    start of flatmates with grup and new group

---
+++ app/src/main/java/com/example/lpras/flatmates/Group.java
@@ -0,0 +1,35 @@
+package com.example.lpras.flatmates;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Group extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        EditText group=(EditText)findViewById(R.id.grpname_editText);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(Group.this,new_group.class));
+
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/lpras/flatmates/new_group.java
@@ -0,0 +1,13 @@
+package com.example.lpras.flatmates;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class new_group extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_group);
+    }
+}
